bd703f9bb2b82b10bfce9534efe4285f2597d297
Wolfgang Mathurin <wmathurin@salesforce.com>
2016-11-03 17:23:53 -0700

    Renamed cordovaHelper.js

--- shared/hybridHelper.js
+++ shared/hybridHelper.js
@@ -5,7 +5,7 @@ var shelljs = require('shelljs');
  *
  * @return {String} The version of the cordova CLI tool, or null if the tool is not installed.
  */
-var getCordovaCliVersion = function() {
+function getCordovaCliVersion() {
 	var cordovaVersionResult = shelljs.exec('cordova -v', { 'silent' : true });
     if (cordovaVersionResult.code !== 0) {
         return null;
