9e32b5a6d0140f4b92dc40f6958137672ceea922
Tim Noonan <tnoonan@salesforce.com>
2019-03-03 15:03:22 -0700

    fix: update dependencies and CI script
    # Conflicts:
    #	sfdx/package.json

--- install.js
+++ install.js
@@ -3,3 +3,6 @@
 var execSync = require('child_process').execSync;
 console.log('Installing npm dependencies');
 execSync('npm install shelljs@0.7.0', {stdio:[0,1,2]});
+execSync('npm install @oclif/command@^1', {stdio:[0,1,2]});
+execSync('npm install @oclif/config@^1', {stdio:[0,1,2]});
+execSync('npm install @salesforce/core@^1.0.1', {stdio:[0,1,2]});
