8f0effb1a2abb232ff9e2f8dffbdb1d2380376cc
Tim Noonan <tnoonan@salesforce.com>
2019-03-04 13:29:19 -0700

    fix: install script can use deps from sfdx/package.json
    # Conflicts:
    #	sfdx/package.json

--- install.js
+++ install.js
@@ -1,6 +1,13 @@
 #!/usr/bin/env node
 
-var execSync = require('child_process').execSync;
+const path = require('path');
+const execSync = require('child_process').execSync;
 console.log('Installing npm dependencies');
-execSync('npm install sfdx/', {stdio:[0,1,2]});
+
+const pjson = require(path.join(__dirname, 'sfdx', 'package.json'));
+
+const deps = Object.entries(pjson.dependencies).map(entry => `${entry[0]}@${entry[1]}`);
+const devDeps = Object.entries(pjson.devDependencies).map(entry => `${entry[0]}@${entry[1]}`);
+
+execSync(`npm install ${[].concat(...deps).concat(...devDeps).join(' ')}`, {stdio:[0,1,2]});
 
