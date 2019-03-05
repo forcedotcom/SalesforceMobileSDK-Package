379eabb22f6a07622a25719e38e784b57b741b3a
Tim Noonan <tnoonan@salesforce.com>
2019-03-05 11:29:20 -0700

    fix: remove global install

--- install.js
+++ install.js
@@ -15,12 +15,6 @@ let devDeps = [];
 if (pjson.devDependencies) {
   devDeps = Object.entries(pjson.devDependencies).map(entry => `${entry[0]}@${entry[1]}`);
 }
-const oclifDevCli = devDeps.find((element) => element.includes('@oclif/dev-cli'));
-
-if (oclifDevCli) {
-  // This is needed globally so the npm prepare script runs during the tests.
-  execSync(`npm install ${oclifDevCli} -g`);
-}
 
 execSync(`npm install ${[].concat(...deps).concat(...devDeps).join(' ')}`, {stdio:[0,1,2]});
 
