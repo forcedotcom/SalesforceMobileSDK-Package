09555afd35341abd4e2ec35e3f677bc431827591
Tim Noonan <tnoonan@salesforce.com>
2019-03-05 09:27:29 -0700

    fix: install script undefined check.

--- install.js
+++ install.js
@@ -6,8 +6,15 @@ console.log('Installing npm dependencies');
 
 const pjson = require(path.join(__dirname, 'sfdx', 'package.json'));
 
-const deps = Object.entries(pjson.dependencies).map(entry => `${entry[0]}@${entry[1]}`);
-const devDeps = Object.entries(pjson.devDependencies).map(entry => `${entry[0]}@${entry[1]}`);
+let deps = [];
+if (pjson.dependencies) {
+  deps = Object.entries(pjson.dependencies).map(entry => `${entry[0]}@${entry[1]}`);
+}
+
+let devDeps = [];
+if (pjson.devDependencies) {
+  devDeps = Object.entries(pjson.devDependencies).map(entry => `${entry[0]}@${entry[1]}`);
+}
 
 execSync(`npm install ${[].concat(...deps).concat(...devDeps).join(' ')}`, {stdio:[0,1,2]});
 
