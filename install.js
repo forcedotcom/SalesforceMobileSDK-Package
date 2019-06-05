7d688bfb2af0e726253cd4ba8695244c8fcecd7a
Wolfgang Mathurin <wmathurin@salesforce.com>
2019-06-04 18:37:25 -0700

    Top level install.js also installs tools needed by release/release.js

--- install.js
+++ install.js
@@ -1,20 +1,20 @@
 #!/usr/bin/env node
 
-const path = require('path');
-const execSync = require('child_process').execSync;
-console.log('Installing npm dependencies');
+const path = require('path')
+const execSync = require('child_process').execSync
+console.log('Installing npm dependencies')
 
-const pjson = require(path.join(__dirname, 'sfdx', 'package.json'));
+let deps = []
+for (let dir of ['sfdx', 'release'].values()) {
+    const pjson = require(path.join(__dirname, dir, 'package.json'))
+    if (pjson.dependencies) {
+        deps = deps.concat(Object.entries(pjson.dependencies).map(entry => `${entry[0]}@${entry[1]}`))
+    }
 
-let deps = [];
-if (pjson.dependencies) {
-  deps = Object.entries(pjson.dependencies).map(entry => `${entry[0]}@${entry[1]}`);
+    if (pjson.devDependencies) {
+        deps = deps.concat(Object.entries(pjson.devDependencies).map(entry => `${entry[0]}@${entry[1]}`))
+    }
 }
 
-let devDeps = [];
-if (pjson.devDependencies) {
-  devDeps = Object.entries(pjson.devDependencies).map(entry => `${entry[0]}@${entry[1]}`);
-}
-
-execSync(`npm install ${[].concat(...deps).concat(...devDeps).join(' ')}`, {stdio:[0,1,2]});
+execSync(`npm install ${deps.join(' ')}`, {stdio:[0,1,2]})
 
