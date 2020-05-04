2834f5b578b32e64154be81d4e74af7179419362
Wolfgang Mathurin <wmathurin@salesforce.com>
2020-05-04 15:53:17 -0700

    Changing supported cocoapod version to 1.8-1.9.1 (before it was 1.7.2-1.9).
    Successfully generated and compiled iOS apps with test_force.js

--- shared/constants.js
+++ shared/constants.js
@@ -48,8 +48,8 @@ module.exports = {
         },
         pod: {
             checkCmd: 'pod --version',
-            minVersion: '1.7.2',
-            maxVersion: '1.9'
+            minVersion: '1.8',
+            maxVersion: '1.9.1'
         },
         cordova: {
             checkCmd: 'cordova -v',
